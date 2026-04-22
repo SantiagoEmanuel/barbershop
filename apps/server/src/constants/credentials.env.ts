6c5afdc3c71964e28a5c51ad70fbbb2072972fc1
santiago mustafa <santiagomustafa3@gmail.com>
2026-04-22 20:33:20 -0300

    chore:  environment variable handling with Zod for validation

--- apps/server/src/constants/credentials.env.ts
+++ apps/server/src/constants/credentials.env.ts
@@ -1,6 +1,15 @@
 import { config } from "dotenv";
+import { coerce, object, string } from "zod";
 
 config();
 
-export const { TURSO_TOKEN, TURSO_URL, PORT, JWT_SECRET, HASH_SALT, NODE_EVN } =
-  process.env;
+const Env = object({
+  PORT: coerce.string(),
+  TURSO_TOKEN: string(),
+  TURSO_URL: string(),
+  JWT_SECRET: string(),
+  HASH_SALT: coerce.number(),
+});
+
+export const { TURSO_TOKEN, TURSO_URL, PORT, JWT_SECRET, HASH_SALT } =
+  Env.parse(process.env);
