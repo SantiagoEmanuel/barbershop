d2db8a6ccd979a1ea4efd03e976abe4ea4abf890
santiago mustafa <santiagomustafa3@gmail.com>
2026-05-25 21:21:21 -0300

    feat(update-prompt): simplify initial state and improve layout for update prompt

--- apps/web/src/components/updatePrompt.tsx
+++ apps/web/src/components/updatePrompt.tsx
@@ -5,7 +5,7 @@ import { useRegisterSW } from "virtual:pwa-register/react";
 
 export function UpdatePrompt() {
   const { needRefresh, updateServiceWorker } = useRegisterSW();
-  const [open, setOpen] = useState<boolean>(needRefresh[0] ?? false);
+  const [open, setOpen] = useState<boolean>(true);
 
   useEffect(() => {
     if (needRefresh[0]) {
@@ -15,11 +15,13 @@ export function UpdatePrompt() {
 
   return (
     <ModalBase onClose={() => setOpen(false)} open={open}>
-      <SectionHeader
-        title="Actualización disponible"
-        description="Puedes actualizar a la última versión del sistema"
-      />
-      <Action action={updateServiceWorker} cancel={() => setOpen(false)} />
+      <div className="flex flex-col gap-8 p-4">
+        <SectionHeader
+          title="Actualización disponible"
+          description="Puedes actualizar a la última versión del sistema"
+        />
+        <Action action={updateServiceWorker} cancel={() => setOpen(false)} />
+      </div>
     </ModalBase>
   );
 }
@@ -27,8 +29,15 @@ export function UpdatePrompt() {
 function Action({ action, cancel }: { action: () => any; cancel: () => void }) {
   return (
     <div className="flex gap-2">
-      <Button onClick={action}>Actualizar</Button>
-      <Button onClick={cancel}>Más Tarde</Button>
+      <Button
+        className="bg-marca text-background cursor-pointer rounded-md px-4 py-2"
+        onClick={action}
+      >
+        Actualizar
+      </Button>
+      <Button className="cursor-pointer px-4 py-2" onClick={cancel}>
+        Más Tarde
+      </Button>
     </div>
   );
 }
