66c773abcc7718e94df90593666a3a15f687ba2f
Santiago Mustafá <santiagomustafa3@gmail.com>
2026-08-18 20:53:58 -0300

    fix(chore): ignore dev-dist to check lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,15 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default tseslint.config(
-  { ignores: ["dist", "**/node_modules", ".husky", "**/generates"] },
+  {
+    ignores: [
+      "dist",
+      "**/node_modules",
+      ".husky",
+      "**/generates",
+      "**/dev-dist",
+    ],
+  },
   {
     files: ["**/*.{ts,tsx}"],
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
